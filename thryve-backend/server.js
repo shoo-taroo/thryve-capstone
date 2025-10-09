e4cdcdd78b257f0d286b29f21e995f4c3a6ef53f
shoo-taroo <liquiganjr@students.national-u.edu.ph>
2025-10-09 19:30:22 +0800

    Added and updated files

--- thryve-backend/server.js
+++ thryve-backend/server.js
@@ -65,3 +65,5 @@ mongoose
     app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
   })
   .catch((err) => console.error('❌ MongoDB connection error:', err));
+
+  module.exports = app;
