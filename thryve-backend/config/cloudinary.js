b07efb4d9e66999c74ad5acbfea1f82b6ef6b0f0
shoo-taroo <liquiganjr@students.national-u.edu.ph>
2025-10-12 17:11:33 +0800

    fixed

--- thryve-backend/config/cloudinary.js
+++ thryve-backend/config/cloudinary.js
@@ -4,7 +4,6 @@ const multer = require('multer');
 
 // 🔹 Configure Cloudinary using environment variables
 cloudinary.config({
-  cloudinary_url: process.env.CLOUDINARY_URL,
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
