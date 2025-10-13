208aa3117e02893f026f23ccdc03ceaae3b59b60
shoo-taroo <liquiganjr@students.national-u.edu.ph>
2025-10-13 13:02:10 +0800

    aasaa

--- thryve-frontend/src/App.jsx
+++ thryve-frontend/src/App.jsx
@@ -72,6 +72,7 @@ const App = () => {
 
           {/* Admin Routes */}
           <Route path="/admin" element={<AdminLayout />}>
+            <Route index element={<AdminInventory />} />
             <Route path="inventory" element={<AdminInventory />} />
             <Route path="feedback" element={<Feedback />} />
           </Route>
